2a03a91f731b764c2fddff5a567f9641077c3dbc
Robert0509 <robert.boroyan@yandex.ru>
2022-10-09 18:34:43 +0300

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,31 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        if (bean == bean1) {
+            System.out.println("true");}
+        else {
+            System.out.println("false");
+        }
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getCat());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        if (bean2 == bean3) {
+            System.out.println("true");}
+        else {
+            System.out.println("false");
+        }
+
+
+
+
     }
 }
